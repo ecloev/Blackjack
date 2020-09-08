b26b01e693fe01058c206a3847f605d53c5c19c0
Ethan Loev <ecloev@outlook.com>
2020-09-07 23:22:04 -0400

    Player Class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Welcome to Blackjack! Enter your name: ");
+        String playerName = scanner.nextLine();
+        var player1 = new Player(playerName);
+        System.out.printf("Player 1: %s, loading your game...", playerName);
     }
 }
