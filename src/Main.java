cd15cb04b8f7e15a4dae96890cb74db12ac1aa54
Ryan Slattery <rlslatte@purdue.edu>
2020-09-07 23:34:08 -0400

    added a comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //this is the main method
         Scanner scanner = new Scanner(System.in);
         System.out.print("Welcome to Blackjack! Enter your name: ");
         String playerName = scanner.nextLine();
