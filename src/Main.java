c3576d9b1ca7ba6732b0f0f502215c3ca3d1ab0b
Ryan Slattery <rlslatte@purdue.edu>
2020-09-26 18:29:28 -0400

    Set up money and wager

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     ArrayList<Card> possibleCards = new ArrayList<Card>();
     ArrayList<Card> deck = new ArrayList<Card>();
+    static int money = 50;
+    static int wager = 0;
 
     public static void main(String[] args) throws InterruptedException { // Execution begins
         Scanner scanner = new Scanner(System.in); // Instantiate scanner object
@@ -13,6 +15,10 @@ public class Main {
         System.out.printf("%s, loading your game.......\n", game.getPlayerName());
         game.deck = Game.shuffle(game.deck);
         Thread.sleep(3000);
+
+        System.out.printf("You currently have $%d.\n", money);
+        System.out.printf("How much would you like to wager:\n");
+
 
 
 
